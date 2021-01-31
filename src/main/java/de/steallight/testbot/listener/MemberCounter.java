1aee1218d818e8fe6b0f377d534b870b7a671a3e
Steallight <69077005+Steallight@users.noreply.github.com>
2021-01-31 22:23:45 +0100

    Update

--- src/main/java/de/steallight/testbot/listener/MemberCounter.java
+++ src/main/java/de/steallight/testbot/listener/MemberCounter.java
@@ -1,5 +1,6 @@
 package de.steallight.testbot.listener;
 
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
@@ -7,15 +8,18 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class MemberCounter extends ListenerAdapter {
     String memberChannel = "805189806639284234";
+    String botChannel = "805545855490064454";
 
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent e) {
 
         int members = e.getGuild().getMemberCount();
-
+        Role botRole = e.getGuild().getRoleById("780068924158509088");
+        VoiceChannel botVC = e.getGuild().getVoiceChannelById(botChannel);
         VoiceChannel vc = e.getGuild().getVoiceChannelById(memberChannel);
-        if (vc != null) {
+        if ((vc != null) && (botVC != null)) {
             vc.getManager().setName("Members: " + members).queue();
+            botVC.getManager().setName("Bots: " + e.getGuild().getMembersWithRoles(botRole).size()).queue();
         }
 
 
