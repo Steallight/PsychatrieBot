ae8bc0537b21d477f1c1ce68f34b9602f6740ad4
Steallight <69077005+Steallight@users.noreply.github.com>
2021-02-07 18:18:30 +0100

    Update

---
+++ src/main/java/de/steallight/testbot/listener/BoostListener.java
@@ -0,0 +1,13 @@
+package de.steallight.testbot.listener;
+
+import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateBoostTimeEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class BoostListener extends ListenerAdapter {
+
+    @Override
+    public void onGuildMemberUpdateBoostTime(GuildMemberUpdateBoostTimeEvent e) {
+
+    }
+
+}
--- src/main/java/de/steallight/testbot/listener/EmoteIdeaListener.java
+++ src/main/java/de/steallight/testbot/listener/EmoteIdeaListener.java
@@ -9,18 +9,24 @@ public class EmoteIdeaListener extends ListenerAdapter {
 
 
     public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
-        e.getMessage().addReaction("\u2705").queue();
+        if (e.getChannel().getId().equals("")) {
+            e.getMessage().addReaction("\u2705").queue();
+        }
     }
 
     public void onMessageReactionAdd(MessageReactionAddEvent e) {
-        if (e.getReaction().getReactionEmote().getName().equals("\u2705")) {
-            if (e.getMember().hasPermission(Permission.MESSAGE_MANAGE)) {
-                if (e.getChannel().getId().equals("804790473016737799")) {
-                    e.getChannel().retrieveMessageById(e.getMessageId()).queue(message -> {
-                        message.delete().queue();
-                    });
-                }
+        if (e.getUser() != null) {
+            if (!e.getUser().isBot()) {
+                if (e.getReaction().getReactionEmote().getName().equals("\u2705")) {
+                    if (e.getMember().hasPermission(Permission.MESSAGE_MANAGE)) {
+                        if (e.getChannel().getId().equals("804790473016737799")) {
+                            e.getChannel().retrieveMessageById(e.getMessageId()).queue(message -> {
+                                message.delete().queue();
+                            });
+                        }
 
+                    }
+                }
             }
         }
     }
