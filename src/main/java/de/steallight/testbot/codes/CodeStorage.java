45b35e9da33de2351f39d5a81a0a876cf7f508e1
Steallight <69077005+Steallight@users.noreply.github.com>
2021-05-12 21:36:39 +0200

    Update

--- src/main/java/de/steallight/testbot/codes/CodeStorage.java
+++ src/main/java/de/steallight/testbot/codes/CodeStorage.java
@@ -4,6 +4,6 @@ public class CodeStorage {
 
     public static String BotToken = "NzY3NDQ4Mzg5ODAxNDc2MDk2.X4yEAg.cUzJP-_d4mZMO8LD4aFqg4cNFv0";
     public static String TwitchClientID = "mqaoxljsgzp7akbp0b2ufzo2sz9fgl";
-    public static String TwitchSecret = "ov3kdqj0fjqbv45hd8gl2weoeo7p8k";
+    public static String TwitchSecret = "wg1w4bsq0nzgj90cxdrnzn46e83o65";
 
 }
